c250415a7907332f03537899cfeed1346c96dc69
JoshKarpel <josh.karpel@gmail.com>
2019-12-17 16:26:06 -0600

    reorganize install script

--- install.sh
+++ install.sh
@@ -4,13 +4,61 @@ function exists_and_not_symlink() {
   [[ (-f $1) && (! -L $1) ]]
 }
 
+function install_conda() {
+  bar
+  curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -fsSL -o $MINICONDA_INSTALLER_PATH
+  bash ~/miniconda-installer.sh -b -p ~/.python
+  bar
+}
+
+function update_conda() {
+  bar
+  conda update -n base --all -y
+  conda clean --all -y
+  bar
+}
+
+function install_rbenv() {
+  bar
+  curl https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer -fsSL | bash
+  bar
+}
+
+function update_ruby() {
+  bar
+  version="$(rbenv install -l | grep -v - | tail -1)"
+  rbenv install -s "$version"
+  rbenv global "$version"
+  bar
+}
+
+function install_rust() {
+  bar
+  curl https://sh.rustup.rs -fsSL | bash -s -- -y --no-modify-path
+  bar
+}
+
+function update_rust() {
+  bar
+  rustup update
+  bar
+}
+
+function install_cargo_packages() {
+  bar
+  cargo install $(cat "$BASEDIR/cargo_install_targets.txt" | xargs)
+  cargo install-update --all
+  bar
+}
+
+echo "executing install script"
+
 BASEDIR="$(dirname "$(realpath -s "$0")")"
 
-echo dotfile directory is $BASEDIR
+echo "dotfile directory is $BASEDIR"
 
 BACKUPS=~/.dotfiles-backups
 
-# make symlinks in the home dir for each files in dotrc
 echo "  creating symlinks in home dir for files in dotrc"
 DOTRC=$BASEDIR/dotrc
 echo "    dotrc directory is $DOTRC"
@@ -33,19 +81,12 @@ done
 source ~/.bashrc
 
 MINICONDA_INSTALLER_PATH=~/miniconda-installer.sh
-# install miniconda
-function install_miniconda() {
-  bar
-  curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -fsSL -o $MINICONDA_INSTALLER_PATH
-  bash ~/miniconda-installer.sh -b -p ~/.python
-  bar
-}
 
 if exists conda; then
   echo "  conda already installed"
 else
   echo "  installing miniconda"
-  install_miniconda
+  install_conda
 fi
 
 # cleanup from miniconda install
@@ -53,21 +94,8 @@ fi
 
 source ~/.bashrc
 
-function update_conda() {
-  bar
-  conda update -n base --all -y
-  conda clean --all -y
-  bar
-}
-
 echo "    updating base conda and cleaning"
 update_conda
-
-function install_rbenv() {
-  bar
-  curl https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer -fsSL | bash
-  bar
-}
 
 if exists rbenv; then
   echo "  rbenv already installed"
@@ -78,23 +106,8 @@ fi
 
 source ~/.bashrc
 
-function update_ruby() {
-  bar
-  version="$(rbenv install -l | grep -v - | tail -1)"
-  rbenv install -s "$version"
-  rbenv global "$version"
-  bar
-}
-
 echo "    updating default ruby version"
 update_ruby
-
-# install rust
-function install_rust() {
-  bar
-  curl https://sh.rustup.rs -fsSL | bash -s -- -y --no-modify-path
-  bar
-}
 
 if exists rustup; then
   echo "  rust already installed"
@@ -103,15 +116,12 @@ else
   install_rust
 fi
 
+echo "    updating rust"
+update_rust
+
 source ~/.bashrc
 
 echo "  installing cargo packages"
-function install_cargo_packages() {
-  bar
-  cargo install $(cat "$BASEDIR/cargo_install_targets.txt" | xargs)
-  cargo install-update --all
-  bar
-}
 
 install_cargo_packages
 
