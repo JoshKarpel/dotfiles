17e1aec66112cbf3d95d2173e639d1575973f51e
JoshKarpel <josh.karpel@gmail.com>
2019-12-18 14:06:46 -0600

    pretty git log!

--- functions/git.sh
+++ functions/git.sh
@@ -12,6 +12,24 @@ function git_repo_is_clean() {
   git diff-index --quiet HEAD
 }
 
+HASH="%C(yellow)%h%C(reset)"
+RELATIVE_TIME="%C(green)%<(15,trunc)%ar%C(reset)"
+AUTHOR="%C(bold blue)%<(15,trunc)%an%C(reset)"
+REFS="%C(bold red)%d%C(reset)"
+SUBJECT="%s"
+
+FORMAT="}$HASH}$RELATIVE_TIME}$AUTHOR}$REFS $SUBJECT"
+
 function pretty_git_log() {
-    echo $PWD
+  git --no-pager log --color=always --pretty=tformat:"$FORMAT" --graph $* |
+    column -t -s '}' |
+    git_page_maybe
+}
+
+function git_page_maybe() {
+  if [ -n "$GIT_NO_PAGER" ]; then
+    cat
+  else
+    less --quit-if-one-screen --no-init --RAW-CONTROL-CHARS --chop-long-lines
+  fi
 }
