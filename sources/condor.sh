4afd9a53ac077ec01927e399d56161c2395c1a39
JoshKarpel <josh.karpel@gmail.com>
2020-02-09 22:37:35 -0600

    add some more aliases

--- sources/condor.sh
+++ sources/condor.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+alias cq="condor_q"
+alias cwq="condor_watch_q"
+alias cs="condor_status"
+
 function check_htmap_usage() {
   condor_q -all -const "IsHTMapJob" $@
 }
--- sources/python.sh
+++ sources/python.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-pyclean() {
+alias p="python"
+
+function clean_python_cache() {
   find . -type f -name "*.py[co]" -print -delete
   find . -type d -name "__pycache__" -print -delete
 }
