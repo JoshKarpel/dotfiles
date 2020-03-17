550ad3ffbeef206e761d6a8ab7c3a05d554932c2
Josh Karpel <josh.karpel@gmail.com>
2020-03-17 10:31:34 -0500

    add chtc jupyter hub servers

--- sources/ssh.sh
+++ sources/ssh.sh
@@ -14,6 +14,9 @@ export sub3="karpel@submit3.chtc.wisc.edu"
 export xfer="karpel@transfer2001.chtc.wisc.edu"
 export subtest="karpel@submittest0000.chtc.wisc.edu"
 
+export jup0="karpel@jupyter0000.chtc.wisc.edu"
+export jup1="karpel@jupyter0001.chtc.wisc.edu"
+
 # MIR servers
 
 export submir="jkarpel@mir-submit.discovery.wisc.edu"
