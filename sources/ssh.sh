693ef9fdcacc01fcf2f8241a0cc8c0d3c74d7499
Josh Karpel <josh.karpel@gmail.com>
2020-03-20 15:43:06 -0500

    reorganize ssh a little

--- sources/ssh.sh
+++ sources/ssh.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-# CS servers
+# CS
 export moria="karpel@moria.cs.wisc.edu"
 
-# CHTC servers
+# FW
 export dumbo="karpel@dumbo.chtc.wisc.edu"
 export batlab="karpel@batlab.chtc.wisc.edu"
 
-# CHTC submit servers
+# CHTC
 export sub1="karpel@submit1.chtc.wisc.edu"
 export sub2="karpel@submit2.chtc.wisc.edu"
 export sub3="karpel@submit3.chtc.wisc.edu"
+
 export xfer2001="karpel@transfer2001.chtc.wisc.edu"
 export subtest="karpel@submittest0000.chtc.wisc.edu"
 
 export jup0="karpel@jupyter0000.chtc.wisc.edu"
 export jup1="karpel@jupyter0001.chtc.wisc.edu"
 
-# MIR servers
-
+# MIR
 export submir="jkarpel@mir-submit.discovery.wisc.edu"
