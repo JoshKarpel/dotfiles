179bb159322d50c72a21422bd30d62d8f09a8b62
Josh Karpel <josh.karpel@gmail.com>
2020-05-09 12:48:38 -0500

    add HT Phenotyping servers to ssh targets

--- sources/ssh.sh
+++ sources/ssh.sh
@@ -18,5 +18,8 @@ export subtest="karpel@submittest0000.chtc.wisc.edu"
 export jup0="karpel@jupyter0000.chtc.wisc.edu"
 export jup1="karpel@jupyter0001.chtc.wisc.edu"
 
+export phenocm="karpel@htpheno-cm.chtc.wisc.edu"
+export phenosub="karpel@htpheno-submit.chtc.wisc.edu"
+
 # MIR
 export submir="jkarpel@mir-submit.discovery.wisc.edu"
