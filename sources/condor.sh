7e4e705b5d8eb5296f7bf5baed8e46589ad2f275
Josh Karpel <josh.karpel@gmail.com>
2020-05-09 13:19:24 -0500

    add condor_where shortcut

--- sources/condor.sh
+++ sources/condor.sh
@@ -7,3 +7,7 @@ alias cs="condor_status"
 function check_htmap_usage() {
   condor_q -all -const "IsHTMapJob" $@
 }
+
+function condor_where() {
+  condor_q $@ -af JobBatchName ClusterId ProcId JobStatus LastRemoteHost
+}
