d043a226f018a6596fd43f732c262f97427a06ce
Josh Karpel <josh.karpel@gmail.com>
2020-05-25 21:36:28 -0500

    add a few new python aliases

--- sources/python.sh
+++ sources/python.sh
@@ -2,6 +2,9 @@
 
 alias py="python"
 alias ipy="ipython"
+alias pc="python -c"
+alias ipc="ipython -i -c"
+alias pm="python -m"
 
 function clean_python_cache() {
   find . -type f -name "*.py[co]" -print -delete
