5a050a362bce531f984e8ca0cb7f4a07885d01d2
Josh Karpel <josh.karpel@gmail.com>
2020-06-24 08:56:48 -0500

    quick function to check gpu lab utilization

--- sources/condor.sh
+++ sources/condor.sh
@@ -9,6 +9,10 @@ function check_htmap_usage() {
   condor_q -all -const "IsHTMapJob" $@
 }
 
+function check_gpulab_utilization() {
+  condor_status -const 'IsGPULabSlot && PartitionableSlot' -af Name GPUs
+}
+
 function condor_where() {
   condor_q $@ -af JobBatchName ClusterId ProcId JobStatus RemoteHost LastRemoteHost
 }
