f6bde6b150d79dbf7234497c84574c5a714b2553
Josh Karpel <josh.karpel@gmail.com>
2020-08-19 14:41:42 -0500

    kubernetes context shortcuts

--- sources/kubernetes.sh
+++ sources/kubernetes.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
 alias k="kubectl"
+
+function ktx() {
+  local ctx=$1
+
+  if [[ -z $ctx ]]; then
+    kubectl config get-contexts
+  else
+    kubectl config use-context $ctx
+  fi
+}
