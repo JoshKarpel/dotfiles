0592362c8daba0989f8fa63b08cf1e0d26867545
Josh Karpel <josh.karpel@gmail.com>
2020-09-07 21:10:26 -0500

    remove CHTC/UW-specific stuff

--- sources/vpn.sh
+++ sources/vpn.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
 
-alias vpn_uw_connect="globalprotect connect --portal uwmadison.vpn.wisc.edu"
-alias vpn_uw_disconnect="globalprotect disconnect"
-alias vpn_uw_status="globalprotect show --status"
