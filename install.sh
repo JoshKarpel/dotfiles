551ec64aa6efd6a85e29bcd1b1a8d4235f526a8e
Josh Karpel <josh.karpel@gmail.com>
2022-04-23 16:25:54 -0500

    --yes

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ function do_apt() {
   fi
 
   log "Setting up Docker repository..."
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
   log "Updating apt targets..."
