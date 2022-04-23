5afe76451b5cfd45ffd0e596ddd9db0808c0d007
Josh Karpel <josh.karpel@gmail.com>
2022-04-23 16:18:09 -0500

    docker repository install

--- install.sh
+++ install.sh
@@ -49,6 +49,10 @@ function do_apt() {
     return 0
   fi
 
+  log "Setting up Docker repository..."
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
   log "Updating apt targets..."
 
   sudo apt update -y
