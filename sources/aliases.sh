a61b26442d0bf40636758f9a3cddcf790f957226
Josh Karpel <josh.karpel@gmail.com>
2026-01-21 16:05:06 -0600

    twiddling

---
+++ sources/aliases.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# auto color on everything
+alias ls="ls --color=auto"
+alias dir="dir --color=auto"
+alias vdir="vdir --color=auto"
+alias grep="grep --color=auto"
+alias fgrep="fgrep --color=auto"
+alias egrep="egrep --color=auto"
+
+# move/copy verbose by default
+alias cp="cp -v"
+alias mv="mv -v"
+
+# rm is always recursive
+alias rm="rm -r"
+
+# mkdir is always verbose and full path
+alias mkdir="mkdir -p"
+
+# cd
+alias ~="cd ~"
+alias ..="cd .."
+alias cd..="cd .." # typos...
+
+# procs
+alias pt="procs -t"
+
+# tar
+alias mktar="tar -cvf"
+alias untar="tar -xvf"
+
+# wsl clock drift
+alias fix-clock-drift="sudo ntpdate -sb time.nist.gov"
+
+# just
+alias j="just"
+
+# watchfiles
+alias w="uvx watchfiles"
