b74fdbbda7d6be880e9eeeda8d2d8faecb5d1c70
Josh Karpel <josh.karpel@gmail.com>
2026-06-14 16:49:56 -0500

    factor out pre-commit helpers

--- sources/pre-commit.sh
+++ sources/pre-commit.sh
@@ -1,31 +1,11 @@
 #!/usr/bin/env bash
 
-function _pre_commit() {
-  if is-uv-project; then
-    uv run pre-commit "$@"
-  else
-    uvx pre-commit "$@"
-  fi
-}
-
-alias pci="_pre_commit install"
-alias pcaa="_pre_commit autoupdate"
-
-function pcr() {
-  local rc
-  git add --update
-  _pre_commit run --show-diff-on-failure "$@" && return 0
-
-  git add --update
-  _pre_commit run --show-diff-on-failure "$@"
-  rc=$?
-  if [[ $rc -ne 0 ]]; then
-    git add --update
-  fi
-  return $rc
-}
+alias pci="pre-commit-run install"
+alias pcaa="pre-commit-run autoupdate"
+alias pcr="pre-commit-autofix --show-diff-on-failure"
+alias pca="pre-commit-autofix --show-diff-on-failure --all-files"
 
 function pcinit() {
-  _pre_commit sample-config > .pre-commit-config.yaml
-  _pre_commit autoupdate
+  pre-commit-run sample-config > .pre-commit-config.yaml
+  pre-commit-run autoupdate
 }
